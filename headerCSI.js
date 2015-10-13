821bc9a54cdbcd46d5eac3cbb02e24b830edfc12
Anthony Ricciardi <vtpasquale@gmail.com>
2015-10-13 18:41:50 -0500

    include google analytics in header CSI

--- headerCSI.js
+++ headerCSI.js
@@ -3,3 +3,6 @@ document.write('<meta name="viewport" content="width=device-width, initial-scale
 document.write('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">');
 document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>');
 document.write('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>');
+
+
+document.write('<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\'); ga(\'create\', \'UA-68818325-1\', \'auto\'); ga(\'send\', \'pageview\');</script>');
