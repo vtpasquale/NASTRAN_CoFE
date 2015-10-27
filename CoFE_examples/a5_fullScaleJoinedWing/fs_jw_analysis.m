3879a54d409234ffb22ac8780f727c9eceb24142
Anthony Ricciardi <vtpasquale@gmail.com>
2015-10-26 22:18:55 -0500

    add and update examples

--- CoFE_examples/a5_fullScaleJoinedWing/fs_jw_analysis.m
+++ CoFE_examples/a5_fullScaleJoinedWing/fs_jw_analysis.m
@@ -5,31 +5,38 @@ home = pwd; cd ..; cd ..; base = pwd; cd(home);
 addpath(fullfile(base,'CoFE_toolbox'))
 
 %% CASE data
-CASE.inputFile = 'fs_jw.bdf';
-CASE.SOL = 103; % SOL = 101, SOL = 103, and SOL = 105 are options
+CASE.inputFile = 'fullJW.bdf';
+CASE.SOL = 103; % SOL = 101, SOL = 103 are options
 CASE.SPC = 1;
 CASE.LOAD = 88;
 CASE.METHOD = 1;
 CASE.RECOVER = 0;
-CASE.WRITE_c06 = 1;
+CASE.WRITE_c06 = 0;
 
 %% Run CoFE
 FEM = CoFE_analysis(CASE);
 
-% %% Read MSC Nastran Results for Comparison
-% switch CASE.SOL
-%     case 101
-%         nas_response = nastran.punchRead('l_static');
-%         nas_comment{1} = 'Linear Static';
-%         nas_scaleOption = 2;
-%     case 103
-%         [nas_response,freq] = nastran.punchRead('modes');
-%         for i = 1:size(freq,2)
-%             nas_comment{i} = sprintf('Vibration Mode %d: %.4f Hz',i,freq(i));
-%         end
-%         nas_scaleOption = ones(size(freq,2));
-% end
-% 
-% %% Plot results
-% post_gui(FEM,nas_response,nas_comment,nas_scaleOption);
-post_gui(FEM)
+%% Read MSC Nastran Results for Comparison
+switch CASE.SOL
+    case 101
+        nas_response = nastran.punchRead('l_static_no_grav');
+        nas_comment{1} = 'Linear Static';
+        nas_scaleOption = 2;
+    case 103
+        [nas_response,freq] = nastran.punchRead('modes');
+        for i = 1:size(freq,2)
+            nas_comment{i} = sprintf('Vibration Mode %d: %.4f Hz',i,freq(i));
+        end
+        nas_scaleOption = ones(size(freq,2));
+    case 105
+        [nas_response,~,ev] = nastran.punchRead('l_buck');
+        nas_comment{1} = 'Linear Static';
+        for i = 1:size(ev,2)-1
+            nas_comment{i+1} = sprintf('Buckling Mode %d: ev = %.4f',i,ev(i+1));
+        end
+        nas_scaleOption = ones(size(ev,2));
+        nas_scaleOption(1) = 2;
+end
+
+%% Plot results
+post_gui(FEM,nas_response,nas_comment,nas_scaleOption);
---
+++ CoFE_examples/a7_gt13ex2/gt13ex2_analysis.m
@@ -0,0 +1,27 @@
+
+%% Clear memory and set path
+clearvars; close all; clc
+home = pwd; cd ..; cd ..; base = pwd; cd(home);
+addpath(fullfile(base,'CoFE_toolbox'))
+
+%% CASE data
+CASE.inputFile = 'gt13ex2.bdf';
+CASE.SOL = 101; % SOL = 101, SOL = 103, and SOL = 105 are options
+CASE.SPC = 100;
+CASE.LOAD = 5;
+CASE.METHOD = 10;
+CASE.RECOVER = 0;
+CASE.WRITE_c06 = 0;
+
+%% Run CoFE
+FEM = CoFE_analysis(CASE);
+
+%% MSC Nastran comparison
+nas_response = nastran.punchRead('gt13ex2');
+nas_comment{1} = 'Linear Static';
+nas_scaleOption = 2;
+
+%% Plot results
+post_gui(FEM,nas_response,nas_comment,nas_scaleOption);
+axis equal
+view(-50,20)
