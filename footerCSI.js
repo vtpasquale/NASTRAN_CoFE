cafb42f47756b030e9043f24ea7981b2173fc27f
Anthony Ricciardi <vtpasquale@gmail.com>
2016-01-30 11:49:12 -0600

    nav and footer updates

--- footerCSI.js
+++ footerCSI.js
@@ -1,5 +1,5 @@
 document.write('<footer class="footer">');
 document.write('<div class="container">');
-document.write('<p class="text-muted">&copy; 2015 Anthony Ricciardi</p>');
+document.write('<p class="text-muted">&copy; 2016 Anthony Ricciardi</p>');
 document.write('</div>');
 document.write('</footer>');
