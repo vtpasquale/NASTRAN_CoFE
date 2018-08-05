5775cd3c7540625f7a568f9755841330c966f2ff
Anthony Ricciardi <vtpasquale@gmail.com>
2018-08-04 23:29:41 -0400

    update case_control and add output_request class

---
+++ cofe_toolbox/model_classes/case_control.m
@@ -0,0 +1,79 @@
+% Class for case control
+% Anthony Ricciardi
+%
+classdef case_control
+    
+    properties
+        ANALYSIS; % [char] Specifies the type of analysis being performed for the current subcase
+
+        TITLE=''; % [char] Defines a title to appear on the first heading line of each page
+        SUBTITLE=''; % [char] Defines a subtitle to appear on the second heading line of each page
+        LABEL=''; % [char] Defines a label to appear on the third heading line of each page
+        
+        SPC % (int > 0 or blank) Selects a single point constraint set to be applied
+        LOAD % (int > 0 or blank) Selects a load set to be applied
+        METHOD % (int > 0 or blank) Selects the real eigenvalue extraction parameters
+        
+        OUTPUT_SETS; % [nsets,1] Array of output_set objects
+        
+        DISPLACEMENT = output_request(); % [output_request] Requests the form and type of nodal displacement output
+        VELOCITY     = output_request(); % [output_request] Requests the form and type of nodal velocity output
+        ACCELERATION = output_request(); % [output_request] Requests the form and type of nodal acceleration output
+        
+        FORCE  = output_request(); % [output_request] Requests the form and type of element force output
+        STRESS = output_request(); % [output_request] Requests the form and type of element stress output
+        STRAIN = output_request(); % [output_request] Requests the form and type of element strain output
+        ESE    = output_request(); % [output_request] Requests the form and type of element strain energy output
+        EKE    = output_request(); % [output_request] Requests the form and type of element kinetic energy output
+    end
+    
+    methods
+       
+        %% Set Methods
+        function obj = set.ANALYSIS(obj,in)
+            if ischar(in)==0; error('case_control.ANALYSIS must be a string'); end
+            opts = {'STATICS','MODES','BUCK'};
+            switch in
+                case opts
+                otherwise
+                    error('Case control ''ANALYSIS = %s'' not supported.',in)
+            end
+            obj.ANALYSIS=in;
+        end
+        function obj = set.TITLE(obj,in)
+            if ischar(in)==0; error('case_control.title must be a string'); end
+            obj.TITLE=in;
+        end
+        function obj = set.SUBTITLE(obj,in)
+            if ischar(in)==0; error('case_control.subtitle must be a string'); end
+            obj.SUBTITLE=in;
+        end
+        function obj = set.LABEL(obj,in)
+            if ischar(in)==0; error('case_control.LABEL must be a string'); end
+            obj.LABEL=in;
+        end
+        function obj = set.OUTPUT_SETS(obj,in)
+            if isa(in,'output_set')==0; error('case_control.OUTPUT_SETS must be an instanct of Class output_set'); end
+            [~,m]=size(in);
+            if m~=1; error('Array instances of Class output_set should have size(output_set,2)==1.'); end
+            obj.OUTPUT_SETS=in;
+        end
+        function obj = set.SPC(obj,in)
+            obj.SPC=setInt(in,'case_control.SPC');
+        end
+        function obj = set.LOAD(obj,in)
+            obj.LOAD=setInt(in,'case_control.LOAD');
+        end
+        function obj = set.METHOD(obj,in)
+            obj.METHOD=setInt(in,'case_control.METHOD');
+        end        
+    end
+    
+end
+
+function out = setInt(in,errStr)
+if isnumeric(in)==0; error([errStr,' must be a number']); end
+if mod(in,1) ~= 0; error([errStr,' must be an integer']); end
+if in < 1; error([errStr,' must be greater than zero.']); end
+out=uint32(in);
+end
---
+++ cofe_toolbox/model_classes/output_request.m
@@ -0,0 +1,82 @@
+% Class for output requests
+% Anthony Ricciardi
+classdef output_request
+    
+    properties
+        all % [logical] Response at all relevant points will be output if true
+        none % [logical] No response will be output if true
+        n % [uint32] Set ID of a defined output_set object. Only response of points that appear on this output_set will be output.
+    end
+    
+    methods
+        function obj = output_request()
+            obj.none = true;
+        end
+        function obj = set.none(obj,in)
+            if ~islogical(in); error('output_request.none must be a logical (true/false) variable.'); end
+            if in == true
+                obj.all = false;
+            end
+            obj.none = in;
+        end
+        function obj = set.all(obj,in)
+            if ~islogical(in); error('output_request.all must be a logical (true/false) variable.'); end
+            if in == true
+                obj.none = false;
+            end
+            obj.all = in;
+        end
+        function obj = set.n(obj,in)
+            if isempty(in)
+                % empty output_request.n is OK
+            else
+                if ~isnumeric(in); error('output_request.n must be numeric or empty variable.'); end
+                if mod(in,1) ~= 0; error('output_request.n must be an integer or empty variable'); end
+                if in < 1; error('output_request.n must be greater than zero.'); end
+                obj.none = false;
+                obj.all = false;
+            end
+            obj.n = uint32(in);
+        end
+        function rind = get_member_ID_indices(obj,IDs,OUTPUT_SETS)
+            % Returns a vector of of indices of input vector IDs(n,1 int)
+            % that correspond to members of the output set. This is useful
+            % for the model data recovery process.
+            [nobj,mobj] = size(obj);
+            if nobj ~= 1 || mobj ~=1
+                error('Method get_member_ID_indices does not support arrays of output_request objects. Method get_member_ID_indices is used only for scalar output_request objects. ');
+            end
+            
+            if obj.none == true
+                % Finished if Request = NONE
+                rind = uint32([]);
+            else
+                [nids,mids]=size(IDs);
+                if nids < 1; error('Metehod input IDs should have size(IDs,1)>0'); end
+                if mids ~= 1; error('Metehod input IDs should have size(IDs,2)=1'); end
+                
+                if obj.all == true
+                    % Finished if Request = ALL
+                    rind = (1:nids).';
+                elseif isempty(obj.n)
+                    error('The output request is undefined.');
+                elseif isempty(OUTPUT_SETS)
+                    error('Input argument OUPUT_SETS cannot be empty.')
+                else
+                    [~,mos]=size(OUTPUT_SETS);
+                    if mos~=1; error('Array instances of Class output_set should have size(output_set,2)==1.'); end
+                    if isa(OUTPUT_SETS,'output_set')==0; error('OUTPUT_SETS must be an instance of Class output_set'); end
+                    output_set_IDs = [OUTPUT_SETS.ID].';
+                    output_set_ID  = find(obj.n==output_set_IDs);
+                    if isempty(output_set_ID)
+                        error('Output Set ID = %d does not exist.',obj.n);
+                    else
+                        % Get vector indicies from referenced output set 
+                        rind = OUTPUT_SETS(output_set_ID).get_member_ID_indices(IDs);
+                    end
+                end
+            end
+        end
+        
+    end
+end
