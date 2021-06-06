838a601878c56a420ec59ee97154e7852a737cee
Anthony Ricciardi <vtpasquale@gmail.com>
2021-06-06 11:03:29 -0400

    file organization

--- testing/system_verification/v1_10_bar_truss_analysis/runTenBarAnalysis.m
+++
@@ -1,8 +0,0 @@
-clearvars; close all; clc
-addpath(genpath(fullfile('..','..','..','nastran_cofe')));
-
-%%
-% cofe = Cofe('tenBarAnalysis.dat','output',false);
-% cofe.solution(1).displacement.printTextOutput(1)
-
-cofe = Cofe('tenBarAnalysis.dat');
