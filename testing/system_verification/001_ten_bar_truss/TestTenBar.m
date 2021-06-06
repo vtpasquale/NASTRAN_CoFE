221b8e96e91c85ea38e1f5170619ee64cd145720
Anthony Ricciardi <vtpasquale@gmail.com>
2021-06-06 11:44:38 -0400

    ten bar truss test case clean up

--- testing/system_verification/001_ten_bar_truss/TestTenBar.m
+++ testing/system_verification/001_ten_bar_truss/TestTenBar.m
@@ -7,15 +7,19 @@ classdef TestTenBar < matlab.unittest.TestCase
             staticSolution = Cofe(fullfile('nastran_runs','static.dat'));
         end
         function compareStatic(testCase)
+            cofeStatic = Hdf5('static.h5');
+            nastranStatic = Hdf5(fullfile('nastran_runs','static.h5'));
             compare = @() cofeStatic.compare(nastranStatic);
-            testCase.verifyThat(compare, IssuesNoWarnings());
+            testCase.verifyWarningFree(compare);
         end
         function runModes(testCase)
-            staticSolution = Cofe(fullfile('nastran_runs','static.dat'));
+            modesSolution = Cofe(fullfile('nastran_runs','static.dat'));
         end
         function compareModes(testCase)
-            compare = @() cofeStatic.compare(nastranStatic);
-            testCase.verifyThat(compare, IssuesNoWarnings());
+            cofeModes = Hdf5('modes.h5');
+            nastranModes = Hdf5(fullfile('nastran_runs','modes.h5'));
+            compare = @() cofeModes.compare(nastranModes);
+            testCase.verifyWarningFree(compare);
         end
     end
     
